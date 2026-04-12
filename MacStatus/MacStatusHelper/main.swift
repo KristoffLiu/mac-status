50a5621f52b6361dee36de12efbc094ae91ce64b
Zhikang Liu <zhikangliu@hotmail.com>
2026-04-13 05:47:46 +0800

    feat: implement SMC-based power monitoring via XPC helper and optimize battery data polling frequency

--- MacStatus/MacStatusHelper/main.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-let delegate = HelperDelegate()
-let listener = NSXPCListener(machServiceName: "com.kristoff.MacStatusHelper")
-listener.delegate = delegate
-listener.resume()
-
-// Keep the daemon alive
-RunLoop.main.run()
