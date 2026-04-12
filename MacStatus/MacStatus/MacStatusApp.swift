6ffac0f80e17a8673246172488580ad43e6b3a26
Zhikang Liu <zhikangliu@hotmail.com>
2026-04-12 23:37:44 +0800

    feat: initialize MacStatus Xcode project and add cross-platform agent skill references

---
+++ MacStatus/MacStatus/MacStatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacStatusApp.swift
+//  MacStatus
+//
+//  Created by kristoff on 12/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct MacStatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
